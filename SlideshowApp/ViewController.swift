209d966fc89098c85f839e6689b94d997b4ebe7d
toaster08 <y10star.hon@gmail.com>
2021-01-02 17:59:36 +0900

    画像拡大・縮小

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    var i:Int = 0
+    var i:Int = 1
     // タイマー
     var timer: Timer!
     // タイマー用の時間のための変数
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
     
     //  次の画像を表示するbotton
     @IBAction func btnNext(_ sender: Any) {
-        if i == 2{ //配列の最後を指定することで自動的に最終を選択
+        if i == 17{ //配列の最後を指定することで自動的に最終を選択
             i = 0
             ansImage.image = UIImage(named:"\(i)")
         }else {
@@ -41,7 +41,7 @@ class ViewController: UIViewController {
     //    前の画像に戻るためのbutton
     @IBAction func btnPreview(_ sender: Any) {
         if i == 0{
-            i = 2
+            i = 17
             ansImage.image = UIImage(named:"\(i)")
         }else {
             i -= 1
@@ -52,7 +52,10 @@ class ViewController: UIViewController {
     // selector: #selector(updatetimer(_:)) で指定された関数
     // timeInterval: 0.1, repeats: true で指定された通り、0.1秒毎に呼び出され続ける
     @objc func updateTimer(_ timer: Timer) {
-        self.timer_sec += 0.1
+        self.timer_sec += 2.0
+        if self.timer_sec.truncatingRemainder(dividingBy: 2.0) == 0{
+         self.btnNext(btnNext as Any)
+        }
     }
     
     @IBOutlet weak var btnAuto: UIButton!
@@ -64,6 +67,7 @@ class ViewController: UIViewController {
             btnAuto.setTitle("一時停止", for:UIControl.State.normal)
             btnNext.isEnabled = false
             btnPreview.isEnabled = false
+
         }else{
             self.timer.invalidate()   // タイマーを停止する
             self.timer = nil          // startTimer() の self.timer == nil で判断するために、 self.timer = nil としておく
@@ -73,4 +77,16 @@ class ViewController: UIViewController {
         }
     }
     
+//  画像タップでの遷移
+    @IBAction func tapAction(_ sender: UIStoryboardSegue) {
+        self.performSegue(withIdentifier: "toSecond", sender: nil)
+    }
+    
+//  遷移の際に画像を送る
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           // segueから遷移先のSecondViewControllerを取得する
+           let secondViewController:SecondViewController = segue.destination as! SecondViewController
+           // 遷移先のSecondViewControllerで宣言しているx, selectedImgに値を代入して渡す
+           secondViewController.selectedImg = UIImage(named:"\(i)")
+    }
 }
